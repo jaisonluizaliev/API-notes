5dd91c44ad91defd40b990862e1e410ca7082bfb
Jaison Dev <jaison.alievi@hotmail.com>
2020-11-19 08:46:49 -0300

    routes added

--- app/controllers/routersNotes.js
+++ app/controllers/routersNotes.js
@@ -11,5 +11,57 @@ async function createdNote(req, res) {
   }
 }
 
+async function downNote(req, res) {
+  try {
+    const { id } = req.params
+    let note = await Note.findById(id)
+    if(isOwner(req.user, note))
+      res.status(200).json(note)
+    else
+      res.status(403).json({ error: "You don't have permission" })
+  } catch (error) {
+    res.status(500).json({error: 'problem to get a note' })
+  }
+}
 
-module.exports = { createdNote } 
+async function showAllNotes(req, res) {
+  try {
+    let notes = await Note.find({author: req.user._id})
+    res.status(200).json(notes)
+  } catch (error) {
+    res.status(500).json({ error: 'problem to show all notes' })
+  }
+}
+
+async function updateNotes(req, res) {
+  const { title, body } = req.body
+  const { id } = req.params
+  
+  try {
+    let note = await Note.findById(id)
+    if(isOwner(req.user, note)) {
+      let note = await Note.findOneAndUpdate(
+        id,
+        {$set: {title, body}},
+        {upsert: true, 'new': true}
+      )
+
+      res.status(200).json(note)
+    } else {
+      res.status(403).json({error: 'permition denied'})
+    }
+  } catch (error) {
+    res.status(500).json({error: 'Problem to update this note!'})
+  }
+}
+
+//method to compare if user is note author
+function isOwner(user, note) {
+  if(JSON.stringify(user._id) == JSON.stringify(note.author._id))
+    return true
+  else 
+    return false
+}
+
+
+module.exports = { createdNote, downNote, showAllNotes, updateNotes } 
--- app/routes/notes.js
+++ app/routes/notes.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const withAuth = require('../middlewares/auth.js')
-const { createdNote } = require('../controllers/routersNotes.js')
+const { createdNote, downNote, showAllNotes, updateNotes } = require('../controllers/routersNotes.js')
 //firstnote
 router.post('/', withAuth , createdNote)
 
+//download note
+router.get('/:id', withAuth, downNote)
+
+//show all notes
+router.get('/', withAuth, showAllNotes)
+
+//update the notes
+router.put('/:id', withAuth, updateNotes)
 
 module.exports = router
