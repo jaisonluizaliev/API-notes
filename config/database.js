57837e1ce54dc999a91b15d606661953391486d2
Jaison Dev <jaison.alievi@hotmail.com>
2020-12-07 11:21:20 -0300

    first deploy api

--- config/database.js
+++ config/database.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise 
 
-mongoose.connect('mongodb://localhost/clone-evernote', {
+require('dotenv').config()
+
+const MONGO_URL = process.env.MONGO_URL
+
+mongoose.connect(MONGO_URL, {
   useNewUrlParser:true,
   useUnifiedTopology:true,
   useCreateIndex: true
